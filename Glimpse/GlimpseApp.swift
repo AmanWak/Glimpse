9627e2fe421ffd2be69375003723e9c108d499d6
AmanWak <awakanka@purdue.edu>
2026-01-30 15:49:02 -0500

    Initial Commit

---
+++ Glimpse/GlimpseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlimpseApp.swift
+//  Glimpse
+//
+//  Created by Aman Wakankar on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GlimpseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlimpseTests/GlimpseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlimpseTests.swift
+//  GlimpseTests
+//
+//  Created by Aman Wakankar on 1/30/26.
+//
+
+import Testing
+@testable import Glimpse
+
+struct GlimpseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
